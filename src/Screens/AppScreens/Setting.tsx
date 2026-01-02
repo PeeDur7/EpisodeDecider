f874292dae33732301ea463bd260733a2a65c2e7
PeeDur7 <petertrann24@gmail.com>
2026-01-01 22:27:52 -0500

    setting page but need styling

--- src/Screens/AppScreens/Setting.tsx
+++ src/Screens/AppScreens/Setting.tsx
@@ -1,14 +1,18 @@
-import { useState } from "react";
-import { StyleSheet, Text, View } from "react-native";
+import { useEffect, useState } from "react";
+import { Pressable, StyleSheet, Text, View } from "react-native";
 import { SafeAreaView } from "react-native-safe-area-context";
 import LogoutButton from "../../Components/LogoutButton";
+import { db, loadUserFromStorage } from "../../Firebase/FirebaseConfig";
+import { doc, getDoc, updateDoc } from "firebase/firestore";
+import { Ionicons } from "@expo/vector-icons";
 
 export default function SettingPage(){
-    const [username, setUsername] = useState("");
-    const [preferedStreamingServices, setPreferedStreamingServices] = useState(""); //set of prefered streaming services
+    const [name, setName] = useState("");
+    const [userId, setUserId] = useState("");
+    const [preferedStreamingServices, setPreferedStreamingServices] = useState<Set<string>>(new Set());
 
     //all of the streaming services this app will create deep links for 
-    const streamingServices = new Set([
+    const streamingServices = [
         "Amazon Prime Video",
         "Amazon Prime Video with Ads",
         "Netflix",
@@ -17,13 +21,89 @@ export default function SettingPage(){
         "Hulu",
         "YouTube TV",
         "Disney Plus"
-    ]);
+    ];
+
+    const addService = async (service : string) => {
+        setPreferedStreamingServices(prev => 
+            new Set(prev).add(service)
+        );
+
+        if(userId){
+            try{
+                await updateDoc(doc(db,"users",userId), {
+                    preferedStreamingServices : Array.from(preferedStreamingServices)
+                });
+            } catch(error) {
+                throw error;
+            }
+        }
+    };
+
+    const removeService = async (service : string) => {
+        setPreferedStreamingServices(prev => {
+            const newSet = new Set(prev);
+            newSet.delete(service);
+            return newSet;
+        })
+        
+        if(userId){
+            try{
+                await updateDoc(doc(db, "users", userId), {
+                    preferedStreamingServices : Array.from(preferedStreamingServices)
+                })
+            }catch(error){
+                throw error;
+            }
+        }
+    };
+
+    //try to get the user's name
+    useEffect(() => {
+        loadUserFromStorage().then(async user => {
+            if(user && user.uid){
+                setUserId(user.uid);
+                const userDoc = await getDoc(doc(db,"users",user.uid));
+                if(userDoc.exists()){
+                    setName(userDoc.data().name);
+                    if(userDoc.data().preferedStreamingService){
+                        setPreferedStreamingServices(new Set(userDoc.data().preferedStreamingService));
+                    }
+                }
+            }
+        })
+    },[]);
 
     return(
         <SafeAreaView style={styles.container} >
             <Text style={styles.title}>Settings</Text>
             <View style={styles.settingsContainer}>
-
+                <Text style={styles.preferedStreamingServicesSubHeading}>Prefered Streaming Services</Text>
+                {
+                    Array.from(preferedStreamingServices).map((service, index) => (
+                        <Pressable
+                            key={index}
+                            onPress={() => removeService(service)}
+                            style={styles.preferedStreamingServicesButton}
+                        >
+                            <Ionicons name="close-outline" size={12} color={"red"}/>
+                            <Text style={styles.preferedStreamingServicesText}>{service}</Text>
+                        </Pressable>
+                    ))
+                }
+                <Text style={styles.streamingServicesSubHeading}>Add Prefered Streaming Services</Text>
+                {
+                    streamingServices.map((service,index) => (
+                        <Pressable
+                            key={index}
+                            onPress={() => addService(service)}
+                            style={styles.streamingServicesButton}
+                        >
+                            <Ionicons name="add-outline" size={12} color={"red"}/>
+                            <Text style={styles.streamingServiceText}>{service}</Text>
+                        </Pressable>
+                    ))
+                }
+                <LogoutButton/>
             </View>
         </SafeAreaView>
     );
@@ -46,5 +126,23 @@ const styles = StyleSheet.create({
         alignItems : "center",
         padding: 20,
         width: "95%"
+    },
+    preferedStreamingServicesSubHeading : {
+
+    },
+    preferedStreamingServicesButton : {
+
+    },
+    preferedStreamingServicesText : {
+
+    },
+    streamingServicesSubHeading : {
+
+    },
+    streamingServicesButton : {
+
+    },
+    streamingServiceText : {
+
     }
 });
